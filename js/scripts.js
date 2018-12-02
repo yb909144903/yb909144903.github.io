87e8b09605771d54fe3eac777af601412c843e51
杨波 <bo.yang02@hand-china.com>
2018-12-02 15:36:04 +0800

    Site updated: 2018-12-02 15:36:02

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@
 
   var s = [
         '<div style="display: none;">',
-          '<script src="https://s11.cnzz.com/z_stat.php?id=1260716016&web_id=1260716016" language="JavaScript"></script>',
+          // '<script src="https://s11.cnzz.com/z_stat.php?id=1260716016&web_id=1260716016" language="JavaScript"></script>',
         '</div>'
       ].join('');
 
